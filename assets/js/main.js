6a4f89b4c77df4348278d2bb775276703b789a12
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-03 13:46:30 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.write("<h1>Hello World</h1>");
+document.write("<p>Have a nice day!</p>");
+let myText = "Hello again<br>";
+document.write(myText);
+
+let vorname = "<br>Sven";
+let nachname = "<br>Güttner";
+document.write(vorname,nachname);
